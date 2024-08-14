9451e4337eb95a752c6bfd93699eabbc59452d30
jek730 <jejek730@gmail.com>
2024-08-14 21:53:12 +0900

    공통 컴포넌트 추가(RequestData, ImageBox), 로딩바 추가, 페이징, 검색 기능 구현

--- src/restaurant/apis/apiInfo.js
+++ src/restaurant/apis/apiInfo.js
@@ -1,27 +1,19 @@
-/* eslint-disable no-undef */
-import apiRequest from '../../commons/libs/apiRequest';
+import requestData from '../../commons/libs/requestData';
 
+// 목록 조회
 export const apiList = (search) => {
   search = search ?? {};
 
   const qs = [];
-  for ([k, v] of Object.entries(search)) {
+  for (const [k, v] of Object.entries(search)) {
     qs.push(`${k}=${v}`);
   }
 
   let url = '/restaurant/list';
-  if (qs.length > 0) url += `?${qs}`;
+  if (qs.length > 0) url += `?${qs.join('&')}`;
 
-  return new Promise((resolve, reject) => {
-    apiRequest(url)
-      .then((res) => {
-        if (res.status === 200) {
-          resolve(res.data.data);
-          return;
-        }
-
-        reject(res.data);
-      })
-      .catch((err) => reject(err));
-  });
+  return requestData(url);
 };
+
+// 상세 조회
+export const apiGet = (seq) => requestData(`/restaurant/info/${seq}`);
--- src/restaurant/containers/ListContainer.js
+++ src/restaurant/containers/ListContainer.js
@@ -1,31 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { apiList } from '../apis/apiInfo';
-import SearchBox from '../components/SearchBox';
 import ItemsBox from '../components/ItemsBox';
+import SearchBox from '../components/SearchBox';
 import Pagination from '../../commons/components/Pagination';
+import Loading from '../../commons/components/Loading';
+
+function getQueryString(searchParams) {
+  const qs = {};
+  if (searchParams.size > 0) {
+    for (const [k, v] of searchParams) {
+      qs[k] = v;
+    }
+  }
+  return qs;
+}
 
 const ListContainer = () => {
-  const [search, setSearch] = useState({});
+  const [searchParams] = useSearchParams();
+
+  const [form, setForm] = useState(() => getQueryString(searchParams));
+  const [search, setSearch] = useState(() => getQueryString(searchParams));
   const [items, setItems] = useState([]);
   const [pagination, setPagination] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     (async () => {
       try {
+        setLoading(true);
         const { items, pagination } = await apiList(search);
         setItems(items);
         setPagination(pagination);
+        setLoading(false);
       } catch (err) {
         console.error(err);
       }
     })();
   }, [search]);
 
+  /* 검색 관련 함수 */
+  const onChangeSearch = useCallback((e) => {
+    setForm((form) => ({ ...form, [e.target.name]: [e.target.value] }));
+  }, []);
+
+  const onSubmitSearch = useCallback(
+    (e) => {
+      e.preventDefault();
+      setSearch({ ...form, page: 1 });
+    },
+    [form],
+  );
+
+  /* 페이지 변경 함수 */
+  const onChangePage = useCallback((p) => {
+    setSearch((search) => ({ ...search, page: p }));
+  }, []);
+
+  // 로딩 처리
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <>
-      <SearchBox search={search} />
+      <SearchBox
+        form={form}
+        onChange={onChangeSearch}
+        onSubmit={onSubmitSearch}
+      />
       <ItemsBox items={items} />
-      <Pagination search={search} />
+      {items.length > 0 && (
+        <Pagination onClick={onChangePage} pagination={pagination} />
+      )}
     </>
   );
 };
