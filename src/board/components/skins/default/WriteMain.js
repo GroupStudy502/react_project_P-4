4e469cc3369c489d83b7f3be5638101667fa9766
jek730 <jejek730@gmail.com>
2024-08-20 13:38:54 +0900

    게시판

---
+++ src/board/components/skins/default/WriteMain.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Form from './Form';
+
+const WriteMain = (props) => {
+  return <Form {...props} />;
+};
+
+export default React.memo(WriteMain);
---
+++ src/routes/Board.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Routs, Route } from 'react-router-dom';
+import loadable from '@loadable/component';
+
+const Board = () => {
+
+};
+
+export default React.memo(Board);
