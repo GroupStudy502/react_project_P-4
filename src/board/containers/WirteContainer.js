34a919e05ad9e42332798359dba012adf86e1bcc
sswum <l5450won@naver.com>
2024-08-20 11:14:35 +0900

    마이페이지 수정 및 게시판 작업중

---
+++ src/board/containers/WirteContainer.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import apiConfig from '../apis/apiConfig';
+import Loading from '../../commons/components/Loading';
+import loadable from '@loadable/component';
+
+function skinRoute( skin, props ) {
+    const WriteMain = loadable (()=> import(`../components/skins/${skin}/WriteMain`),
+ );
+
+ return <WriteMain {...props} />;
+}
+
+const WriteContainer = ({ setPageTitle }) => {
+  const [board, setBoard] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { bid } = useParams();
+
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+
+        const data = await apiConfig(bid);
+        setBoard(data); // 게시판 설정 조회
+        setPageTitle(data.bname); // 사이트 제목
+
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, [bid, setPageTitle]);
+
+  if (loading || !board) {
+    return <Loading />;
+  }
+  
+  const { skin } = board;
+
+
+  return skinRoute(skin, { board });
+};
+
+export default React.memo(WriteContainer);
