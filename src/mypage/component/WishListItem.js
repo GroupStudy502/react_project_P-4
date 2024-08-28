72815b67d6102c7f7f902f16b441bb73a6ccf004
sswum <l5450won@naver.com>
2024-08-28 20:01:19 +0900

    찜목록 스타일 구현중

--- src/mypage/component/WishListItem.js
+++ src/mypage/component/WishListItem.js
@@ -5,13 +5,30 @@ const Wrapper = styled.ul`
 
 border : 3px;
 
+
 `;
 
 const ItemBox = ({ item, className }) => {
   return <li className={className}>{item.rstrNm}</li>;
 };
 
-const StyledItemBox = styled(ItemBox)``;
+const StyledItemBox = styled(ItemBox)`
+
+  background-color: gray;
+ /* 공통 스타일 */
+  outline-color: black;
+  border: 3px;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  inline-size: 100px;
+  margin-top: 20px;
+  margin-left: 20px;
+  width: 300px;
+
+`;
 
 const WishListItem = ({ items }) => {
   console.log(items);
