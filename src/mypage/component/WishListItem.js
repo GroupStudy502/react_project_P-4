21eedf118ac7c1e775cfbbdb99b345240e1e7894
sswum <l5450won@naver.com>
2024-08-28 17:54:19 +0900

    찜하기 목록 작업 처리

--- src/mypage/component/WishListItem.js
+++ src/mypage/component/WishListItem.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-const WishListItem = ({ items, pagination }) => {
-  console.log(items, pagination);
-  return <></>;
+const Wrapper = styled.ul`
+
+border : 3px;
+
+`;
+
+const ItemBox = ({ item, className }) => {
+  return <li className={className}>{item.rstrNm}</li>;
+};
+
+const StyledItemBox = styled(ItemBox)``;
+
+const WishListItem = ({ items }) => {
+  console.log(items);
+  return (
+
+   
+    <Wrapper>
+      {items &&
+        items.length > 0 &&
+        items.map((item) => <StyledItemBox key={item.rstrId} item={item} />)}
+    </Wrapper>
+    
+  );
 };
 
 export default React.memo(WishListItem);
