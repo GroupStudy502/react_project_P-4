4d80a490cb919ddaf65260de6dd1a68d1b299305
decl5th <alsgur6624@gmail.com>
2024-08-30 21:10:29 +0900

    NoDataText 따로 빼기, 리뷰 기본 스타일링

---
+++ src/restaurant/components/NoDataText.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const NoDataText = styled.div`
+    display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 16px;
+  color: #818181;
+`;
