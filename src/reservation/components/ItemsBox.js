9343ad59fb20c147351203ce194e8a330bbca2ae
lionjeongin <cindy93489163@gmail.com>
2024-08-30 17:07:04 +0900

    rem 철자 오류 수정 중

--- src/reservation/components/ItemsBox.js
+++ src/reservation/components/ItemsBox.js
@@ -4,12 +4,31 @@ import styled from 'styled-components';
 import { FaRegCalendarCheck } from 'react-icons/fa';
 import { ImageBgBox } from '../../commons/components/ImageBox';
 
+
+const ResaddressWithIcon = styled.h2`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px; /* 아이콘+글씨 줄 아래 마진 */
+  margin-top: 30px; /* 아이콘+글씨 줄 위 마진 */
+
+  svg {
+    margin-right: 7px; /* 아이콘과 글씨 사이 간격 */
+    font-size: 1.1em; /* 아이콘 크기 */
+  }
+
+  h2 {
+    margin: 0; /* Remove default margin from h2 */
+    font-size: 0.8em; /* h2 글씨 크기(...선택해 주세요) */
+    font-weight: bold;
+  }
+`;
+
 const Resname = styled.div`
   font-weight: bold;
   font-size: 1.7em;
 `;
 
-const Resadress = styled.div`
+const Resaddress = styled.div`
   font-weight: bold;
   font-size: 1em;
   color: #666;
@@ -53,38 +72,42 @@ const ItemBox = ({ item, className }) => {
   const {
     restaurant: { images, rstrNm },
   } = item;
+
   return (
     <li className={className}>
-      {images && images.length > 0 && (
-        <ImageBgBox
-          className="photo"
-          url={images[0].rstrImgUrl}
-          alt={rstrNm}
-          width="150px"
-          height="150px"
-        />
-      )}
-      <div className="item-content">
-        <ResorderNo>
-          <div className="orderNo">
-            <FaRegCalendarCheck />
-            <strong> </strong>
-            {item?.orderNo}
-          </div>
-        </ResorderNo>
-        <Resname>
-          <div className="rname">{item?.rname}</div>
-        </Resname>
-        <Resadress>
-          <div className="raddress">{item?.raddress}</div>
-        </Resadress>
-        <Resinfo>
-          <div className="rDateTime">{item?.rDateTime}</div>
-          <div className="persons">{item?.persons}명</div>
-        </Resinfo>
-        <Link to={url}>
-          <ReservationButton>예약 정보</ReservationButton>
-        </Link>
+      <div className="item-inner">
+        {images && images.length > 0 && (
+          <ImageBgBox
+            className="photo"
+            url={images[0].rstrImgUrl}
+            alt={rstrNm}
+            width="150px"
+            height="150px"
+          />
+        )}
+        <div className="item-content">
+          <ResorderNo>
+            <div className="orderNo">
+              <ResaddressWithIcon>
+                <FaRegCalendarCheck />
+                <h2>{item?.orderNo}번</h2>
+              </ResaddressWithIcon>
+            </div>
+          </ResorderNo>
+          <Resname>
+            <div className="rname">{item?.rname}</div>
+          </Resname>
+          <Resaddress>
+            <div className="raddress">{item?.raddress}</div>
+          </Resaddress>
+          <Resinfo>
+            <div className="rDateTime">{item?.rDateTime}</div>
+            <div className="persons">{item?.persons}명</div>
+          </Resinfo>
+          <Link to={url}>
+            <ReservationButton>예약 정보</ReservationButton>
+          </Link>
+        </div>
       </div>
     </li>
   );
@@ -96,15 +119,24 @@ const ItemStyledBox = styled(ItemBox)`
   box-shadow: 2px 2px 5px #818181;
   border-radius: 5px;
 
-  a {
+  .item-inner {
     display: flex;
+    align-items: center;
+  }
+
+  .photo {
+    margin-right: 20px;
+    flex-shrink: 0;
+  }
+
+  .item-content {
+    width: 100%;
+    word-break: break-all;
+  }
+
+  a {
     text-decoration: none;
     color: inherit;
-
-    .item-content {
-      width: 100%;
-      word-break: break-all;
-    }
   }
 `;
 
